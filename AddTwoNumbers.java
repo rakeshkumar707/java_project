3918f6dc30a97d3107a0ae773744631f88393fb5
raaj <raaj@gmail.com>
2024-02-28 22:49:05 +0530

    java_file commited

---
+++ AddTwoNumbers.java
@@ -0,0 +1,11 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        double num1 = 5.5;
+        double num2 = 10.0;
+
+        double sum = num1 + num2;
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
