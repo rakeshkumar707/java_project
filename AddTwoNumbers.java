05c3b78469076baf9a987ac21ab0c78fd6b3c0f9
raaj <raaj@gmail.com>
2024-03-05 10:31:51 +0530

    new

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         double num1 = 5;
-        double num2 = 10.0;
+        double num2 = 10;
 
         double sum = num1 + num2;
 
